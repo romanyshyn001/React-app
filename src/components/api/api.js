575536f38dcd3a3ceadc7263c3aca7303a01f6cb
unknown <roman1romanyshyn@gmail.com>
2022-10-03 14:39:01 +0300

    deploy website

--- src/components/api/api.js
+++ src/components/api/api.js
@@ -1,64 +1,55 @@
-import axios from "axios"
-import { follow, unfollow } from "../../redux/usersReducers"
+import axios from "axios";
 
 const instance = axios.create({
-  withCredentials:true,
-  baseURL:`https://social-network.samuraijs.com/api/1.0/`,
-  headers:{
-    "API-KEY":"add5d5d4-dea4-4fa0-816f-6d976a407dfa",
-    "Content-Type": "application/json"
-  }
-})
+  withCredentials: true,
+  baseURL: `https://social-network.samuraijs.com/api/1.0/`,
+  headers: {
+    "API-KEY": "351871fe-5294-484c-9371-af232b81c222",
+    "Content-Type": "application/json",
+  },
+});
 
-export const  usersAPI = {
-   getUsers: async(currentPage = 1, pageSize = 10) => { 
-      return await instance.get(`users?page=${currentPage}&count=${pageSize}`, 
-      )
-      .then(response => {
-        return response.data
-      })
-},
-   unfollow: async(userId ) => {  
-      return await instance.delete(`follow/${userId}`)
-         // .then(response => {
-         //    if(response.data.resultCode === 0){
-         //       unfollow(userId)
-         //    }
-         // })
-},
-   follow: async(userId) => {
-      return await instance.post(`follow/${userId}`)
-         // .then(response => {
-         //    if(response.data.resultCode === 0){
-         //       follow(userId)
-         //    }
-         // })
-},
-   getProfile: async(userId) => {
-      return await profileAPI.getProfile(userId)
-   }
-}
+export const usersAPI = {
+  getUsers: async (currentPage = 1, pageSize = 10) => {
+    return await instance
+      .get(`users?page=${currentPage}&count=${pageSize}`)
+      .then((response) => {
+        return response.data;
+      });
+  },
+  unfollow: async (userId) => {
+    return await instance.delete(`follow/${userId}`);
+  },
+  follow: async (userId) => {
+    return await instance.post(`follow/${userId}`);
+  },
+  getProfile: async (userId) => {
+    return await profileAPI.getProfile(userId);
+  },
+};
 
 export const profileAPI = {
-   getProfile: async(userId) => {
-      return await instance.get(`profile/`+ userId)
-   },
-   getStatus: async(userId) => {
-      return await instance.get(`profile/status/` + userId)
-   },
-   updateStatus: async(status) => {
-      return await instance.put(`profile/status`, {status:status})
-   }
-}
+  getProfile: async (userId) => {
+    return await instance.get(`profile/` + userId);
+  },
+  getStatus: async (userId) => {
+    return await instance.get(`profile/status/` + userId);
+  },
+  updateStatus: async (status) => {
+    return await instance.put(`profile/status`, { status: status });
+  },
+};
 
 export const authAPI = {
-   async me(){
-      return await instance.get(`auth/me`) 
+  async me() {
+    return await instance.get(`auth/me`);
   },
-   async login(values){
-      return await instance.post(`auth/login`, values)
+  async login(values) {
+    return await instance.post(`auth/login`, values);
   },
-   async logout(){
-      return await instance.delete(`auth/login`).then(res => { return res.data})
-}
-}
+  async logout() {
+    return await instance.delete(`auth/login`).then((res) => {
+      return res.data;
+    });
+  },
+};
