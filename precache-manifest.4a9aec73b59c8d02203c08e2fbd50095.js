dd277fa4711392c8b66a009cd677aa6cfdb52d3f
unknown <roman1romanyshyn@gmail.com>
2023-01-10 13:30:58 +0200

    Updates

---
+++ precache-manifest.4a9aec73b59c8d02203c08e2fbd50095.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "30e7f93abedc6dddf580",
+    "url": "/React-app/static/css/main.126b01ba.chunk.css"
+  },
+  {
+    "revision": "30e7f93abedc6dddf580",
+    "url": "/React-app/static/js/main.30e7f93a.chunk.js"
+  },
+  {
+    "revision": "1e0bafea56ba7a216e9a",
+    "url": "/React-app/static/css/1.533adbc4.chunk.css"
+  },
+  {
+    "revision": "1e0bafea56ba7a216e9a",
+    "url": "/React-app/static/js/1.1e0bafea.chunk.js"
+  },
+  {
+    "revision": "45eebac8b08363d858da",
+    "url": "/React-app/static/css/2.581786ff.chunk.css"
+  },
+  {
+    "revision": "45eebac8b08363d858da",
+    "url": "/React-app/static/js/2.45eebac8.chunk.js"
+  },
+  {
+    "revision": "d9977bc367573a23c2fc",
+    "url": "/React-app/static/js/3.d9977bc3.chunk.js"
+  },
+  {
+    "revision": "b915b9cc59dd1f4b6cb1",
+    "url": "/React-app/static/js/runtime~main.b915b9cc.js"
+  },
+  {
+    "revision": "73de705eeb12bb05ed0333b917c16e87",
+    "url": "/React-app/static/media/userPhoto.73de705e.png"
+  },
+  {
+    "revision": "a3529090a59472cac199e5ffd82ade0a",
+    "url": "/React-app/static/media/preloader.a3529090.gif"
+  },
+  {
+    "revision": "6429af1b63f16c5509fdaf363053e863",
+    "url": "/React-app/static/media/avaMessage.6429af1b.png"
+  },
+  {
+    "revision": "6429af1b63f16c5509fdaf363053e863",
+    "url": "/React-app/static/media/ava.6429af1b.png"
+  },
+  {
+    "revision": "798cc8c8b9ab1a1fc05d9e30f98c3e2e",
+    "url": "/React-app/index.html"
+  }
+];
